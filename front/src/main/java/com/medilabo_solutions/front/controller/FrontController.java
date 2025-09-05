ff9707b46596cbb5e15cdaa3e93623d80ba93822
Fethi KHARRAT <fethi.kharrat@gmail.com>
2025-09-05 11:53:13 +0200

    securityConfig dans gateway

--- front/src/main/java/com/medilabo_solutions/front/controller/FrontController.java
+++ front/src/main/java/com/medilabo_solutions/front/controller/FrontController.java
@@ -1,6 +1,10 @@
 package com.medilabo_solutions.front.controller;
 
 import com.medilabo_solutions.front.model.Patient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +18,18 @@ public class FrontController {
     @GetMapping("/front/patients")
     public String getPatients(Model model) {
         RestTemplate restTemplate = new RestTemplate();
-        Patient[] patients = restTemplate.getForObject(gatewayUrl +"/patients", Patient[].class);
-        model.addAttribute("patients", patients);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("admin", "password"); // credentials du gateway
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Patient[]> response = restTemplate.exchange(
+                gatewayUrl + "/patients",
+                HttpMethod.GET,
+                entity,
+                Patient[].class
+        );
+        model.addAttribute("patients", response.getBody());
+
         return "patients"; // fichier patients.html
     }
 }
---
+++ gateway-service/src/main/java/com/medilabo_solutions/gateway_service/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.medilabo_solutions.gateway_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    // Création d'un utilisateur en mémoire
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("admin")
+                .password("{noop}password") // {noop} = pas d’encodage
+                .roles("ADMIN")
+                .build();
+
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    // Configuration de la sécurité
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable) // désactive CSRF
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/login", "/logout").permitAll() // accès libre
+                        .anyExchange().authenticated() // tout le reste nécessite login
+                )
+                .httpBasic(Customizer.withDefaults()) // Auth Basic (Postman / navigateur)
+                .formLogin(Customizer.withDefaults()) // Formulaire login si accès depuis navigateur
+                .build();
+    }
+}
